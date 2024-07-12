6e1bde6c5fa6926dc0bc02c7991a778bdf583bab
Jesús Retana <retanaj@globalhitss.com>
2024-07-11 22:54:17 -0600

    test cambio a vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import cloudflare from "@astrojs/cloudflare";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare()
+  adapter: vercel()
 });
